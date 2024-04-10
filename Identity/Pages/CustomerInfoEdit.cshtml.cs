20e7b24ee5eefd73d287769db8753cd14e94c1f9
Shulin Zhang <zslinco@byu.edu>
2024-04-10 12:13:57 -0600

    Add Admin Views

---
+++ Identity/Pages/CustomerInfoEdit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INTEXII.Pages
+{
+    public class CustomerInfoEditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Identity/Pages/ProductEdit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INTEXII.Pages
+{
+    public class ProductEditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
