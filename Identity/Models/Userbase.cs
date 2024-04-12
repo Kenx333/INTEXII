b8dbf62d376fcfcbb444f30657dd0261e71a7c10
Shulin Zhang <zslinco@byu.edu>
2024-04-12 08:30:22 -0600

    Update

--- Identity/Models/Userbase.cs
+++ Identity/Models/Userbase.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace INTEXII.Models
 {
     public class Userbase
     {
         [Required]
+        [Key]
         public int ProductionRecommondation_ID { get; set; }
+
+        [ForeignKey("Product") ]
         public string name { get; set; }
 
         public string ProductRecm1 { get; set; }
